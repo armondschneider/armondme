276e5faf01aba473cf5ae86f28eacc7d9c7f1684
armondschneider <mondoloves@icloud.com>
2025-07-28 07:55:16 -0700

    .

--- app/work/[id]/page.tsx
+++ app/work/[id]/page.tsx
@@ -1,5 +1,15 @@
 import JobExperienceDetail from '../../components/JobExperienceDetail';
 
+// Generate static params for all work pages
+export function generateStaticParams() {
+  return [
+    { id: 'paradox' },
+    { id: 'dump' },
+    { id: 'hause' },
+    { id: 'personal' }
+  ];
+}
+
 export default function WorkDetailPage() {
   return <JobExperienceDetail />;
 }
